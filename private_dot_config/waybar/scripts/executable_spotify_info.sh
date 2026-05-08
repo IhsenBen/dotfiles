c4917deeb8431cffcc99f253cd0b063420ccf4a0
Ihsen Ben Ali <ihsen.benali@riedel.net>
2026-05-08 11:20:29 +0200

    Update .config/waybar/scripts/spotify_info.sh

--- private_dot_config/waybar/scripts/executable_spotify_info.sh
+++ private_dot_config/waybar/scripts/executable_spotify_info.sh
@@ -7,5 +7,5 @@ if ! playerctl -p spotify status &>/dev/null; then
     exit 0
 fi
 
-# Use stdbuf to disable output buffering and compact JSON format
-stdbuf -oL playerctl -p spotify metadata --format '{"text":"{{artist}} - {{title}}","tooltip":"{{album}} - {{artist}} - {{title}}","alt":"{{status}}","class":"{{status}}"}' -F 2>/dev/null
+# Use stdbuf to disable output buffering and compact JSON format with markup escaping
+stdbuf -oL playerctl -p spotify metadata --format '{"text":"{{markup_escape(artist)}} - {{markup_escape(title)}}","tooltip":"{{markup_escape(album)}} - {{markup_escape(artist)}} - {{markup_escape(title)}}","alt":"{{status}}","class":"{{status}}"}' -F 2>/dev/null
